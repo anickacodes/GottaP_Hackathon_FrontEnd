91dacb0b42672808cb5488893f7efdfbf564f53c
keniaDE <decoteaukenia@gmail.com>
2024-02-03 15:30:04 -0500

    navbar feature + bootstrap install

--- gottap_front/src/App.jsx
+++ gottap_front/src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
-<>
-<h1> App </h1></>
+    <NavBar />
   )
 }
 
