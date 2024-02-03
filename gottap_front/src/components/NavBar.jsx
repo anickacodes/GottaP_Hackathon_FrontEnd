f064c2beda284ffb768f470e26508a9f93fe4976
anickacodes <anickalewis@pursuit.org>
2024-02-03 12:14:17 -0500

    component + pages setup

---
+++ gottap_front/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+
+const NavBar = () => {
+
+    return (
+        <nav className="NavBar-page">
+            <h2> NavBar</h2>
+        </nav>
+    )
+}
+
+export default NavBar
---
+++ gottap_front/src/pages/About.jsx
@@ -0,0 +1,12 @@
+
+
+const About = () => {
+
+    return (
+        <div className="about-page">
+            <h2> About</h2>
+        </div>
+    )
+}
+
+export default About
