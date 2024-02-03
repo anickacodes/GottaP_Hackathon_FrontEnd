80f3ed5af4e1ad7b4624fed1b1349b78e2f52b65
anickacodes <anickalewis@pursuit.org>
2024-02-03 16:22:22 -0500

    imports into App

--- gottap_front/src/App.jsx
+++ gottap_front/src/App.jsx
@@ -2,19 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import FooterBar from './components/FooterBar.jsx'
 import GoogleMapComponent from "./components/GoogleMapComponent";
 
 function App() {
   return (
     <div>
-      <h1> App </h1>
+      <NavBar />
       <GoogleMapComponent />
+      <FooterBar />
     </div>
   );
-import NavBar from "./components/NavBar";
-
-function App() {
-  return <NavBar />;
-}
+  }
 
 export default App;
