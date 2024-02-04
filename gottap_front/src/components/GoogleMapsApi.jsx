9fcbb1d129a474a94b8c7433645d8ac5413d7717
anickacodes <anickalewis@pursuit.org>
2024-02-04 16:17:00 -0500

    fix final

--- gottap_front/src/components/GoogleMapsApi.jsx
+++ gottap_front/src/components/GoogleMapsApi.jsx
@@ -1,82 +1,93 @@
-
 import {
-    GoogleMap,
-    Marker,
-    LoadScript,
-    InfoWindow
-  } from "@react-google-maps/api";
+  GoogleMap,
+  Marker,
+  LoadScript,
+  InfoWindow,
+} from "@react-google-maps/api";
 
-  const containerStyle = {
-    width: "800px",
-    height: "600px",
+
+
+const center = {
+  lat: 40.7128,
+  lng: -74.006,
+};
+
+const containerStyle = {
+  width: "800px",
+  height: "600px",
+};
+
+const GoogleMapsComponent = ({
+  bathrooms,
+  userLocation,
+  selectedBathroom,
+  setSelectedBathroom,
+}) => {
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  console.log(selectedBathroom);
+
+  const handleMarkerClick = (bathroom) => {
+    setSelectedBathroom(bathroom);
   };
-
-  const GoogleMapsComponent = ({ bathrooms, userLocation, selectedBathroom, setSelectedBathroom }) => {
-    const API_KEY = import.meta.env.VITE_API_KEY;
-    console.log(selectedBathroom)
-
-    const handleMarkerClick = (bathroom) => {
-        setSelectedBathroom(bathroom);
-      };
-// hover effect instead of click on marker
-// once clicked, pan into 
-      const handleCloseInfoWindow = () => {
-        setSelectedBathroom(null);
-      };
-      bathrooms.map(bathroom => {
-        console.log(bathrooms)
-        console.log("Bathroom marker position:", {
-          lat: bathroom.latitude,
-          lng: bathroom.longitude
-        })
-    })
-   ;
-      return (
-      <LoadScript googleMapsApiKey={API_KEY} loading="async">
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
-        {
-            bathrooms.map(bathroom => (
-                
-                <Marker
-                key={bathroom.id}
-                position={{
-                    lat: bathroom.latitude,
-                    lng: bathroom.longitude  
-                }}
-                title={bathroom.name}
-                onClick={() => handleMarkerClick(bathroom)}
-                />
-            ))
-        }
-{selectedBathroom && (
+  // hover effect instead of click on marker
+  // once clicked, pan into
+  const handleCloseInfoWindow = () => {
+    setSelectedBathroom(null);
+  };
+  bathrooms.map((bathroom) => {
+    console.log(bathrooms);
+    console.log("Bathroom marker position:", {
+      lat: bathroom.latitude,
+      lng: bathroom.longitude,
+    });
+  });
+  return (
+    <LoadScript googleMapsApiKey={API_KEY} loading="async">
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+        {bathrooms.map((bathroom) => (
+          <Marker
+            key={bathroom.id}
+            position={{
+              lat: bathroom.latitude,
+              lng: bathroom.longitude,
+            }}
+            title={bathroom.name}
+            onClick={() => handleMarkerClick(bathroom)}
+          />
+        ))}
+        {selectedBathroom && (
           <InfoWindow
             position={{
               lat: selectedBathroom.latitude,
-              lng: selectedBathroom.longitude
+              lng: selectedBathroom.longitude,
             }}
             onCloseClick={handleCloseInfoWindow}
           >
-            <div>
+            <div >
               <h3>{selectedBathroom.name}</h3>
               {selectedBathroom && (
                 <div>
-          <h4>Unisex: {selectedBathroom.unisex ? 'True' : 'False'}</h4>
-          <h4>Address: {selectedBathroom.street}, {selectedBathroom.city}, {selectedBathroom.state}</h4>
-          <h4>Directions: {selectedBathroom.directions}, Distance: {selectedBathroom.distance.toFixed(2)} Miles</h4>
+                  <h4>Unisex: {selectedBathroom.unisex ? "True" : "False"}</h4>
+                  <h4>
+                    Address: {selectedBathroom.street}, {selectedBathroom.city},{" "}
+                    {selectedBathroom.state}
+                  </h4>
+                  <h4>
+                    Directions: {selectedBathroom.directions}, Distance:{" "}
+                    {selectedBathroom.distance.toFixed(2)} Miles
+                  </h4>
                 </div>
               )}
             </div>
           </InfoWindow>
         )}
+      </GoogleMap>
+    </LoadScript>
+  );
+};
 
-        </GoogleMap>
-      </LoadScript>
-        )
-  }
+export default GoogleMapsComponent;
 
-export default GoogleMapsComponent
-
-  
 //   {selectedBathroom && (
 //     <InfoWindow
 //       position={{
