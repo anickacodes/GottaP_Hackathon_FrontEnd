c23ee5196b7311e374f50046b0d0e996fe2ff307
Jesse Chong <jessechong@pursuit.org>
2024-02-04 13:26:51 -0500

    both apis connected w/ markers

--- gottap_front/src/App.jsx
+++ gottap_front/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import RestRoomApi from './components/RestRoomApi'
 import './App.css'
 
 function App() {
 
   return (
-<>
-<h1> App </h1></>
+<div>
+<h1> App </h1>
+<RestRoomApi />
+</div>
   )
 }
 
