79340a7a7b2fb761a8f0796eb81733ca37a08ff9
anickacodes <anickalewis@pursuit.org>
2024-02-04 14:54:32 -0500

    fixed imports

--- gottap_front/src/components/NavBar.jsx
+++ gottap_front/src/components/NavBar.jsx
@@ -1,18 +1,22 @@
 import "../components/NavBar.css"
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-
+import logo from "../assets/output-onlinegiftools(4).gif"
+import NavbarBrand from "react-bootstrap/esm/NavbarBrand";
 
 
 const NavBar = () => {
 
     return (
-        <Navbar className="border" expand="lg" >
-            <Container >
 
-                <Navbar.Brand className="mx-auto" >
-                    <span className="font-weight-bold display-2" >GottaP</span></Navbar.Brand>
-
+        <Navbar bg="dark" variant="dark"  >
+            <Container className="contain">
+                <NavbarBrand className="mx-auto" expand="lg" >
+                    <img src={logo}
+                        alt="Logo"
+                        className="img "
+                    />
+                </NavbarBrand>
             </Container>
         </Navbar>
     )
